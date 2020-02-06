48d9143c1f125787d0fac0803a01f3103817770a
Abhishek Dubey <abhi.dubey011999@gmail.com>
2020-02-06 22:25:50 +0530

    j5lte: AEX bringup
    - Removed other ROMs makefiles
    - Added common lunch choices
    
    Signed-off-by: Abhishek Dubey <abhi.dubey011999@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,8 @@
 LOCAL_PATH := device/samsung/j5lte
+
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/omni_j5lte.mk \
-	$(LOCAL_DIR)/rr_j5lte.mk \
-	$(LOCAL_DIR)/pa_j5lte.mk \
-	$(LOCAL_DIR)/lineage_j5lte.mk
+	$(LOCAL_DIR)/aex_j5lte.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_j5lte-userdebug \
+    aosp_j5lte-eng
